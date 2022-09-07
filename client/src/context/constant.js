bda2fb9f32422d3a53a450cc201dae9683688d37
haotranhuynhhuy <tranhuynhhuyhao@gmail.com>
2022-09-07 15:51:20 +0700

    Prepare Deloy

--- client/src/context/constant.js
+++ client/src/context/constant.js
@@ -1,3 +1,6 @@
-export const apiUrl = process.env.NODE_ENV !== 'production' ? 'http://localhost:5000/api' : 'xxx';
+export const apiUrl =
+  process.env.NODE_ENV !== "production"
+    ? "http://localhost:5000/api"
+    : "https://obscure-brook-56796.herokuapp.com/api";
 
-export const LOCAL_STORAGE_TOKEN_NAME = 'token-mern'
+export const LOCAL_STORAGE_TOKEN_NAME = "token-mern";
