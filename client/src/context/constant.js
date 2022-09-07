4aed46cea4ebf5064c0d2477a0eae826582bc1ad
haotranhuynhhuy <tranhuynhhuyhao@gmail.com>
2022-09-07 16:50:48 +0700

    Prepare Deploy

--- client/src/context/constant.js
+++ client/src/context/constant.js
@@ -1,6 +1,6 @@
 export const apiUrl =
   process.env.NODE_ENV !== "production"
-    ? "https://lit-tundra-13416.herokuapp.com/api"
-    : "";
+    ? "http://localhost:5000/api"
+    : "https://lit-tundra-13416.herokuapp.com/api";
 
 export const LOCAL_STORAGE_TOKEN_NAME = "token-mern";
